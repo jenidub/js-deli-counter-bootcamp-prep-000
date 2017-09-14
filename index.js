5e9a7cc4382d34897476c53c3d5ec98fb6799973
Jeni Williams <stanfordjeni@gmail.com>
2017-09-14 04:22:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, customer) {
+  katzDeliLine.push(customer)
+  return katzDeliLine.length
+}
+
+function nowServing(){
+  if (katzDeliLine.length > 0) {
+    var person = katzDeliLine[0]
+    katzDeliLine.unshift()
+    return person
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
