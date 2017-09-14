fda23384ccffafab9a7bb980d43010541a74a172
Jeni Williams <stanfordjeni@gmail.com>
2017-09-14 16:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ function takeANumber(katzDeliLine, customer) {
   return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`
 }
 
-/*function currentLine(line) {
-  var numbers = []
-  for (var i = 0; i < line.length; i++) {
-    numbers.push(i)
-  }
+function currentLine(line) {
   if(line.length > 0) {
-    console.log(`The line is currently: ${} `)
+    var printout = []
+    for (var i = 0; i < line.length; i++) {
+      printout.push(i+1 + ". " + line[i])
+    }
+    return "The line is currently: " + printout.join(" ")
   } else {
     return "The line is currently empty."
   }
-}*/
+}
 
 function nowServing(line){
   if (line.length > 0) {
