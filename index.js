89009c719d9cbf48d12736390404f90ad6cd7572
Jeni Williams <stanfordjeni@gmail.com>
2017-09-14 04:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = []
 
 function takeANumber(katzDeliLine, customer) {
   katzDeliLine.push(customer)
-  return `Welcome, ${customer}.  You are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(){
