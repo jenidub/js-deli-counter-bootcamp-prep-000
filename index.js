ef5dccb3054ff9f86cc67e43247842111d598f67
Jeni Williams <stanfordjeni@gmail.com>
2017-09-14 16:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function takeANumber(katzDeliLine, customer) {
 
 function nowServing(line){
   if (line.length > 0) {
-    return `Currently serving ${line[0]}`
+    return `Currently serving ${line[0]}.`
     line.shift()
   } else {
     return "There is nobody waiting to be served!"
